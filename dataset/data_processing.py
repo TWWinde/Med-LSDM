f6bdb93448cf5cdf9a1075f36437e0b5cf0f03bb
TWWinde <twwinde@gmail.com>
2024-05-31 15:35:13 +0200

    Update data_processing.py

--- dataset/data_processing.py
+++ dataset/data_processing.py
@@ -21,6 +21,7 @@ def save_cropped(files, folder, crop_size, crop_2_block=False, length=32, stride
     for file_path in files:
         img = nib.load(file_path)
         data = img.get_fdata()
+        print(data.shape)
         if crop_2_block:
             for i in range(0, data.shape[2]-length, stride):
                 cropped_data = crop_block(data, *crop_size, i, length)
