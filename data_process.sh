508dbdba5dd8b93eff756d9d3e1f501487039a69
TWWinde <twwinde@gmail.com>
2024-06-03 14:54:57 +0200

    Update data_process.sh

--- data_process.sh
+++ data_process.sh
@@ -5,12 +5,13 @@
 #SBATCH --output=PROCESS%j.%N.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=6-23:00:00
+#SBATCH --time=3-23:00:00
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-#SBATCH --gpus=rtx_a5000:1
+#SBATCH --qos=shortbatch
+#SBATCH --partition=highperf
 
 # Activate everything you need
 
