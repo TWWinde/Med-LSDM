4d939b0dce935d3c1efa14eecf79f96683415873
TWWinde <twwinde@gmail.com>
2024-06-18 13:42:27 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -26,18 +26,18 @@ python -c "import torch; print(torch.__version__)"
 # Run your python code
 
 # autopet
-#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
-#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
-#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
-
-# autopet_wo_artifacts
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet_wo_artifacts \
+PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
 model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# autopet_wo_artifacts
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet_wo_artifacts \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # mr_wo_artifacts
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023 \
