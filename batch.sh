c818d329ba92d514e6ce5d5cba32002ea966ac6e
TWWinde <twwinde@gmail.com>
2024-06-21 11:42:32 +0200

    update

--- batch.sh
+++ batch.sh
@@ -47,14 +47,14 @@ python -c "import torch; print(torch.__version__)"
 #model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # mr_wo_mask
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023_wo_mask \
-model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
-model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023_wo_mask \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # diffusion
-#python train/train_ddpm.py model=ddpm dataset=brats model.results_folder_postfix='flair' \
-#model.vqgan_ckpt='/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/BRATS/flair/lightning_logs/version_0/checkpoints/latest_checkpoint.ckpt' \
-#model.diffusion_img_size=32 model.diffusion_depth_size=32 \
-#model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=10 model.gpus=1
+python train/train_ddpm.py model=ddpm dataset=autpet model.results_folder_postfix='flair' \
+model.vqgan_ckpt='/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/AutoPET/flair/lightning_logs/version_133784/checkpoints/latest_checkpoint.ckpt' \
+model.diffusion_img_size=32 model.diffusion_depth_size=32 \
+model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=10 model.gpus=1
