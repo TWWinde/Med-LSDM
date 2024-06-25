a481efe398298bdf3139fd970def8fb751e37ce3
TWWinde <twwinde@gmail.com>
2024-06-25 11:48:46 +0200

    dataloader

--- dataset/synthrad2023.py
+++ dataset/synthrad2023.py
@@ -2,6 +2,7 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torchio as tio
 import os
+import nibabel as nib
 
 
 class RandomDepthCrop:
@@ -16,6 +17,14 @@ class RandomDepthCrop:
         cropped_img = img[:, d_start:d_start + target_d, :, :]
 
         return cropped_img
+
+
+def normalization(data):
+    arr_min = data.min()
+    arr_max = data.max()
+    data_normalized = (data - arr_min) / (arr_max - arr_min)
+    data_normalized = data_normalized * 2 - 1
+    return data_normalized
 
 
 PREPROCESSING_TRANSORMS = tio.Compose([
@@ -57,6 +66,39 @@ class SynthRAD2023Dataset(Dataset):
         return {'data': img.data.permute(0, -1, 1, 2)}
 
 
+class SynthRAD2023Datase1(Dataset):
+    def __init__(self, root_dir: str):
+        super().__init__()
+        self.root_dir = root_dir
+        self.preprocessing = PREPROCESSING_TRANSORMS
+        self.transforms = TRAIN_TRANSFORMS
+        self.file_paths = self.get_data_files()
+
+    def get_data_files(self):
+        subfolder_names = os.listdir(self.root_dir)
+        folder_names = [os.path.join(
+            self.root_dir, subfolder, 'mr.nii.gz') for subfolder in subfolder_names ] # if subfolder.endswith('.nii.gz')]
+        return folder_names
+
+    def image_process(self, data):
+        img = RandomDepthCrop(data)
+
+
+
+
+        return img
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx: int):
+
+        img = nib.load(self.file_paths[idx])
+
+        img = self.image_process(img)
+        return {'data': img.data.permute(0, -1, 1, 2)}
+
+
 if __name__ == '__main__':
     dataset = SynthRAD2023Dataset(root_dir = '')
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
