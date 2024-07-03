1921b6914632ca6cd2bb39c0a9784dc785f687d5
TWWinde <twwinde@gmail.com>
2024-07-03 11:17:43 +0200

    Update diffusion.py

--- ddpm/diffusion.py
+++ ddpm/diffusion.py
@@ -1221,6 +1221,315 @@ class GaussianDiffusion(nn.Module):
 
         return self.p_losses(x, t, *args, **kwargs)
 
+
+class SemanticGaussianDiffusion(nn.Module):
+    def __init__(
+            self,
+            denoise_fn,
+            *,
+            image_size,
+            num_frames,
+            text_use_bert_cls=False,
+            channels=3,
+            timesteps=1000,
+            loss_type='l1',
+            use_dynamic_thres=False,  # from the Imagen paper
+            dynamic_thres_percentile=0.9,
+            vqgan_ckpt=None,
+    ):
+        super().__init__()
+        self.channels = channels
+        self.image_size = image_size
+        self.num_frames = num_frames
+        self.denoise_fn = denoise_fn  # Unet3D noise_predictor
+
+        if vqgan_ckpt:
+            self.vqgan = VQGAN.load_from_checkpoint(vqgan_ckpt).cuda()
+            self.vqgan.eval()
+        else:
+            self.vqgan = None
+
+        betas = cosine_beta_schedule(timesteps)
+
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.)
+
+        timesteps, = betas.shape
+        self.num_timesteps = int(timesteps)
+        self.loss_type = loss_type
+
+        # register buffer helper function that casts float64 to float32
+
+        def register_buffer(name, val):
+            return self.register_buffer(
+                name, val.to(torch.float32))
+
+        register_buffer('betas', betas)
+        register_buffer('alphas_cumprod', alphas_cumprod)  # alphas_t_bar
+        register_buffer('alphas_cumprod_prev', alphas_cumprod_prev)  # alphas_t-1_bar
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+
+        register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod))  # sqrt(alphas_t_bar)
+        register_buffer('sqrt_one_minus_alphas_cumprod',
+                        torch.sqrt(1. - alphas_cumprod))  # sqrt(1-alphas_t_bar)
+        register_buffer('log_one_minus_alphas_cumprod',
+                        torch.log(1. - alphas_cumprod))  # log(1-alphas_t_bar)
+        register_buffer('sqrt_recip_alphas_cumprod',
+                        torch.sqrt(1. / alphas_cumprod))  # sqrt(1/alphas_t_bar)
+        register_buffer('sqrt_recipm1_alphas_cumprod',
+                        torch.sqrt(1. / alphas_cumprod - 1))  # sqrt((1/alphas_t_bar)-1)
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+
+        posterior_variance = betas * \
+                             (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+        # above: equal to 1. / (1. / (1. - alpha_cumprod_tm1) + alpha_t / beta_t)
+
+        register_buffer('posterior_variance', posterior_variance)
+
+        # below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
+
+        register_buffer('posterior_log_variance_clipped',
+                        torch.log(posterior_variance.clamp(min=1e-20)))
+        register_buffer('posterior_mean_coef1', betas *
+                        torch.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod))
+        register_buffer('posterior_mean_coef2', (1. - alphas_cumprod_prev)
+                        * torch.sqrt(alphas) / (1. - alphas_cumprod))
+
+        # text conditioning parameters
+
+        self.text_use_bert_cls = text_use_bert_cls
+
+        # dynamic thresholding when sampling
+
+        self.use_dynamic_thres = use_dynamic_thres
+        self.dynamic_thres_percentile = dynamic_thres_percentile
+
+    def q_sample(self, x_start, t, noise=None):  # x_{t}
+        """
+            Diffuse the data for a given number of diffusion steps.
+
+            In other words, sample from q(x_t | x_0).
+
+            :param x_start: the initial data batch.
+            :param t: the number of diffusion steps (minus 1). Here, 0 means one step.
+            :param noise: if specified, the split-out normal noise.
+            :return: A noisy version of x_start.
+        """
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        return (
+                extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start +
+                extract(self.sqrt_one_minus_alphas_cumprod,
+                        t, x_start.shape) * noise
+        )
+
+    def q_mean_variance(self, x_start, t):  # q(x_{t} | x_0)
+        """
+        Get the distribution q(x_t | x_0).
+
+        :param x_start: the [N x C x ...] tensor of noiseless inputs.
+        :param t: the number of diffusion steps (minus 1). Here, 0 means one step.
+        """
+        mean = extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start
+        variance = extract(1. - self.alphas_cumprod, t, x_start.shape)
+        log_variance = extract(
+            self.log_one_minus_alphas_cumprod, t, x_start.shape)
+        return mean, variance, log_variance
+
+    def predict_start_from_noise(self, x_t, t, noise):  # x_{0} = f(x_{t}, noise, t)
+
+        return (
+                extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t -
+                extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * noise
+        )
+
+    def q_posterior(self, x_start, x_t, t):
+        """
+        Compute the mean and variance of the diffusion posterior:
+
+                 q(x_{t-1} | x_t, x_0)
+
+        """
+        posterior_mean = (
+                extract(self.posterior_mean_coef1, t, x_t.shape) * x_start +
+                extract(self.posterior_mean_coef2, t, x_t.shape) * x_t
+        )
+        posterior_variance = extract(self.posterior_variance, t, x_t.shape)
+        posterior_log_variance_clipped = extract(
+            self.posterior_log_variance_clipped, t, x_t.shape)
+        return posterior_mean, posterior_variance, posterior_log_variance_clipped
+
+    def p_mean_variance(self, x, seg, t, clip_denoised: bool, cond=None, cond_scale=1.):
+        """
+        Apply the model to get p(x_{t-1} | x_t), as well as a prediction of
+        the initial x, x_0.
+
+        :param x: the [N x C x ...] tensor at time t.
+        :param t: a 1-D Tensor of timesteps.
+        :param clip_denoised: if True, clip the denoised signal into [-1, 1].
+        :param denoised_fn: if not None, a function which applies to the
+                   x_start prediction before it is used to sample. Applies before
+                   clip_denoised.
+
+        :return: a dict with the following keys:
+                    - 'mean': the model mean output.
+                    - 'variance': the model variance output.
+                    - 'log_variance': the log of 'variance'.
+         """
+        x_recon = self.predict_start_from_noise(
+            x, t=t, noise=self.denoise_fn.forward_with_cond_scale(x, seg,  t, cond=cond, cond_scale=cond_scale))
+        # elf.denoise_fn == Unet3D   x_recon == x_{0}
+        if clip_denoised:
+            s = 1.
+            if self.use_dynamic_thres:
+                s = torch.quantile(
+                    rearrange(x_recon, 'b ... -> b (...)').abs(),
+                    self.dynamic_thres_percentile,
+                    dim=-1
+                )
+
+                s.clamp_(min=1.)
+                s = s.view(-1, *((1,) * (x_recon.ndim - 1)))
+
+            # clip by threshold, depending on whether static or dynamic
+            x_recon = x_recon.clamp(-s, s) / s
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(
+            x_start=x_recon, x_t=x, t=t)  # q(x_{t-1} |x_{t}, x_0)
+        return model_mean, posterior_variance, posterior_log_variance
+
+    @torch.inference_mode()  # add noise
+    def p_sample(self, x, seg, t, cond=None, cond_scale=1., clip_denoised=True):
+        """
+        Sample x_{t-1} from the model at the given timestep. from noise
+
+        :param model: the model to sample from.
+        :param x: the current tensor at x_{t-1}.
+        :param t: the value of t, starting at 0 for the first diffusion step.
+        :param denoised_fn: if not None, a function which applies to the
+            x_start prediction before it is used to sample.
+
+        """
+        b, *_, device = *x.shape, x.device
+        model_mean, _, model_log_variance = self.p_mean_variance(
+            x=x, seg=seg, t=t, clip_denoised=clip_denoised, cond=cond, cond_scale=cond_scale)
+        noise = torch.randn_like(x)
+        # no noise when t == 0
+        nonzero_mask = (1 - (t == 0).float()).reshape(b,
+                                                      *((1,) * (len(x.shape) - 1)))
+        return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
+
+    @torch.inference_mode()
+    def p_sample_loop(self, shape, cond=None, cond_scale=1.):
+        """
+            Generate samples from the model. From noise to x_{t}
+
+        """
+
+        device = self.betas.device
+
+        b = shape[0]
+        img = torch.randn(shape, device=device)
+
+        for i in tqdm(reversed(range(0, self.num_timesteps)), desc='sampling loop time step', total=self.num_timesteps):
+            img = self.p_sample(img, torch.full(
+                (b,), i, device=device, dtype=torch.long), cond=cond, cond_scale=cond_scale)
+
+        return img
+
+    @torch.inference_mode()
+    def sample(self, cond=None, cond_scale=1., batch_size=16):
+        """
+           from latent space to image space
+
+        """
+        device = next(self.denoise_fn.parameters()).device
+
+        if is_list_str(cond):
+            cond = bert_embed(tokenize(cond)).to(device)
+
+        batch_size = cond.shape[0] if exists(cond) else batch_size
+        image_size = self.image_size
+        channels = self.channels
+        num_frames = self.num_frames
+        _sample = self.p_sample_loop(
+            (batch_size, channels, num_frames, image_size, image_size), cond=cond, cond_scale=cond_scale)
+
+        if isinstance(self.vqgan, VQGAN):
+            # denormalize TODO: Remove eventually
+            _sample = (((_sample + 1.0) / 2.0) * (self.vqgan.codebook.embeddings.max() -
+                                                  self.vqgan.codebook.embeddings.min())) + self.vqgan.codebook.embeddings.min()
+
+            _sample = self.vqgan.decode(_sample, quantize=True)
+        else:
+            unnormalize_img(_sample)
+
+        return _sample
+
+    @torch.inference_mode()
+    def interpolate(self, x1, x2, t=None, lam=0.5):
+        b, *_, device = *x1.shape, x1.device
+        t = default(t, self.num_timesteps - 1)
+
+        assert x1.shape == x2.shape
+
+        t_batched = torch.stack([torch.tensor(t, device=device)] * b)
+        xt1, xt2 = map(lambda x: self.q_sample(x, t=t_batched), (x1, x2))
+
+        img = (1 - lam) * xt1 + lam * xt2
+        for i in tqdm(reversed(range(0, t)), desc='interpolation sample time step', total=t):
+            img = self.p_sample(img, torch.full(
+                (b,), i, device=device, dtype=torch.long))
+
+        return img
+
+    def p_losses(self, x_start, seg,  t, cond=None, noise=None, **kwargs):
+        b, c, f, h, w, device = *x_start.shape, x_start.device
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)  # x_{t}
+
+        if is_list_str(cond):
+            cond = bert_embed(
+                tokenize(cond), return_cls_repr=self.text_use_bert_cls)
+            cond = cond.to(device)
+
+        x_recon = self.denoise_fn(x_noisy, seg, t, cond=cond, **kwargs)  # predicted noise
+
+        if self.loss_type == 'l1':
+            loss = F.l1_loss(noise, x_recon)
+        elif self.loss_type == 'l2':
+            loss = F.mse_loss(noise, x_recon)
+        else:
+            raise NotImplementedError()
+
+        return loss
+
+    def forward(self, x, *args, **kwargs):
+        if isinstance(self.vqgan, VQGAN):
+            with torch.no_grad():
+                x = self.vqgan.encode(
+                    x, quantize=False, include_embeddings=True)
+                # normalize to -1 and 1
+                x = ((x - self.vqgan.codebook.embeddings.min()) /
+                     (self.vqgan.codebook.embeddings.max() -
+                      self.vqgan.codebook.embeddings.min())) * 2.0 - 1.0
+        else:
+            print("Hi")
+            x = normalize_img(x)  # (-1,1)
+
+        b, device, img_size, = x.shape[0], x.device, self.image_size
+        check_shape(x, 'b c f h w', c=self.channels,
+                    f=self.num_frames, h=img_size, w=img_size)
+        t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
+
+        return self.p_losses(x, t, *args, **kwargs)
+
+
 # trainer class
 
 
