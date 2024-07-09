9d7f17ef761cb75fc8a1c0bbdc8e561593b216af
TWWinde <twwinde@gmail.com>
2024-07-09 12:39:06 +0200

    update

--- ddpm/semantic_diffusion.py
+++ ddpm/semantic_diffusion.py
@@ -1325,7 +1325,8 @@ class Semantic_Trainer(object):
                     plt.imshow(frame[0], cmap='gray')
                     plt.savefig(path)
 
-                self.save(milestone)
+                if self.step != 0 and self.step % (self.save_and_sample_every * 5) == 0:
+                    self.save(milestone)
 
             log_fn(log)
             self.step += 1
