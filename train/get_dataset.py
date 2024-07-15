1d9e6cb73827f6f677ebc89c74ae4e4c4a89d4d7
TWWinde <twwinde@gmail.com>
2024-07-15 12:37:59 +0200

    Update get_dataset.py

--- train/get_dataset.py
+++ train/get_dataset.py
@@ -3,8 +3,7 @@ from torch.utils.data import WeightedRandomSampler
 
 
 def get_dataset(cfg):
-    print(cfg.dataset.name)
-    print("Model is trained on ", cfg.model)
+    print("Model is trained on ", cfg.dataset.name)
     if cfg.dataset.name == 'MRNet':
         train_dataset = MRNetDataset(
             root_dir=cfg.dataset.root_dir, task=cfg.dataset.task, plane=cfg.dataset.plane, split='train')
