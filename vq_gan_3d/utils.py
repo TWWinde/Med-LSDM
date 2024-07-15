493512e3f7281cbe684e2b8a71503aed322ff70e
TWWinde <twwinde@gmail.com>
2024-07-15 16:13:09 +0200

    Update utils.py

--- vq_gan_3d/utils.py
+++ vq_gan_3d/utils.py
@@ -122,12 +122,13 @@ def adopt_weight(global_step, threshold=0, value=0.):
 ###########
 def save_video_grid(video, fname, nrow=None, fps=6):
     b, c, t, h, w = video.shape
-    print(video.shape)
-    if c != 1:
-        video = torch.argmax(video, dim=1).unsqueeze(1)
 
     video = video.permute(0, 2, 3, 4, 1)
     video = (video.cpu().numpy() * 255).astype('uint8')
+    if c != 1:
+        video = np.argmax(video, axis=4, keepdims=True)
+        c = video.shape[4]
+
     if nrow is None:
         nrow = math.ceil(math.sqrt(b))
     ncol = math.ceil(b / nrow)
