7c88882e6092a3ca62f61757b8ac93d9b30f96e8
TWWinde <twwinde@gmail.com>
2024-07-15 22:36:04 +0200

    Update diffusion.sh

--- diffusion.sh
+++ diffusion.sh
@@ -45,7 +45,7 @@ python train/train_ddpm.py model=ddpm dataset=autopet model.results_folder_postf
 model.vqgan_ckpt='/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/AutoPET1/flair/lightning_logs/version_133784/checkpoints/latest_checkpoint.ckpt' \
 model.seggan_ckpt="/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/SemanticMap/results/lightning_logs/version_136248/checkpoints/latest_checkpoint.ckpt"
 model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
-model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=4 model.gpus=0
+model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0
 
 # diffusion without condition
 #python train/train_ddpm.py model=ddpm dataset=synthrad2023_wo_mask model.results_folder_postfix="output' model.name=vq_gan_3d \
