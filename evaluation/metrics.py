23b934f270bae7b50cc14803e54b408cda2620fe
TWWinde <twwinde@gmail.com>
2024-07-18 20:53:02 +0200

    Update metrics.py

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -11,8 +11,21 @@ import torch.nn.functional as F
 # --------------------------------------------------------------------------#
 
 
-class metrics:
-    def __init__(self, path,  dataloader_val):
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor(
+        [np.exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)]
+    )
+    return gauss / gauss.sum()
+
+
+def create_window(window_size, channel=1):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = _2D_window.expand(channel, 1, window_size, window_size, window_size).contiguous()
+    return window
+
+class Metrics:
+    def __init__(self, path, dataloader_val):
         self.val_dataloader = dataloader_val
         self.path_to_save = path
         self.path_to_save_PIPS = os.path.join(self.path_to_save, "PIPS")
@@ -32,7 +45,7 @@ class metrics:
         total_samples = len(self.val_dataloader)
         with torch.no_grad():
             for i, data_i in enumerate(self.val_dataloader):
-                image,  label = self.preprocess_input(data_i)
+                image, label = self.preprocess_input(data_i)
                 generated = model.sample(cond=label)
                 input1 = (generated + 1) / 2
                 input2 = (image + 1) / 2
@@ -54,10 +67,6 @@ class metrics:
         avg_ssim = sum(ssim) / total_samples
         avg_psnr = sum(psnr) / total_samples
         avg_rmse = sum(rmse) / total_samples
-        avg_pips = np.array(avg_pips)
-        avg_ssim = np.array(avg_ssim)
-        avg_psnr = np.array(avg_psnr)
-        avg_rmse = np.array(avg_rmse)
 
         return avg_pips, avg_ssim, avg_psnr, avg_rmse
 
@@ -70,11 +79,9 @@ class metrics:
             total_loss += loss_lpips(img1[:, :, i, :, :], img2[:, :, i, :, :])
         return total_loss / d
 
-    def psnr_3d(self, img1, img2, max_pixel_value=1.0):
-        max_pixel_value = max(max(img1), max(img2))
+    def psnr_3d(self, img1, img2):
         mse = torch.mean((img1 - img2) ** 2)
-        psnr = 20 * torch.log10(max_pixel_value / torch.sqrt(mse))
-
+        psnr = 20 * torch.log10(1.0 / torch.sqrt(mse))
         return psnr.item()
 
     def rmse_3d(self, img1, img2):
@@ -120,7 +127,6 @@ class metrics:
             return ssim_map.mean(1).mean(1).mean(1), cs
 
     def preprocess_input(self, data):
-
         # move to GPU and change data types
         label = data['label'].cuda().long()
         img = data['image'].cuda().float()
@@ -134,7 +140,7 @@ class metrics:
 
     def update_metrics(self, model, cur_iter):
         print("--- Iter %s: computing PIPS SSIM PSNR RMSE---" % (cur_iter))
-        cur_pips, cur_ssim, cur_psnr, cur_rmse= self.compute_metrics(model)
+        cur_pips, cur_ssim, cur_psnr, cur_rmse = self.compute_metrics(model)
         self.update_logs(cur_pips, cur_iter, 'PIPS')
         self.update_logs(cur_ssim, cur_iter, 'SSIM')
         self.update_logs(cur_psnr, cur_iter, 'PSNR')
@@ -164,63 +170,11 @@ class metrics:
         plt.close()
 
     def metrics_test(self, model):
-        print("--- test: computing FID ---")
-        pips, ssim, psnr, rmse = self.compute_metrics(model.module.netG, model.module.netEMA, model)
+        print("--- test: computing PIPS SSIM PSNR RMSE ---")
+        pips, ssim, psnr, rmse = self.compute_metrics(model)
         print("--- PIPS at test : ", "{:.2f}".format(pips))
         print("--- SSIM at test : ", "{:.5f}".format(ssim))
         print("--- PSNR at test : ", "{:.2f}".format(psnr))
         print("--- RMSE at test : ", "{:.2f}".format(rmse))
 
 
-
-
-def gaussian(window_size, sigma):
-    gauss = torch.Tensor(
-        [np.exp(-(x - window_size // 2)**2 / float(2 * sigma**2)) for x in range(window_size)]
-    )
-    return gauss / gauss.sum()
-
-
-def create_window(window_size, channel=1):
-    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
-    _3D_window = (_1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0))
-    window = _3D_window.expand(channel, 1, window_size, window_size, window_size).contiguous()
-    return window
-
-
-def ssim_3d(img1, img2, window_size=11, size_average=True, val_range=None):
-    if val_range is None:
-        max_val = 255 if torch.max(img1) > 128 else 1
-        min_val = -1 if torch.min(img1) < -0.5 else 0
-        L = max_val - min_val
-    else:
-        L = val_range
-
-    padd = 0
-    (_, channel, depth, height, width) = img1.size()
-    window = create_window(window_size, channel=channel).to(img1.device)
-
-    mu1 = F.conv3d(img1, window, padding=padd, groups=channel)
-    mu2 = F.conv3d(img2, window, padding=padd, groups=channel)
-
-    mu1_sq = mu1.pow(2)
-    mu2_sq = mu2.pow(2)
-    mu1_mu2 = mu1 * mu2
-
-    sigma1_sq = F.conv3d(img1 * img1, window, padding=padd, groups=channel) - mu1_sq
-    sigma2_sq = F.conv3d(img2 * img2, window, padding=padd, groups=channel) - mu2_sq
-    sigma12 = F.conv3d(img1 * img2, window, padding=padd, groups=channel) - mu1_mu2
-
-    C1 = (0.01 * L) ** 2
-    C2 = (0.03 * L) ** 2
-
-    v1 = 2.0 * sigma12 + C2
-    v2 = sigma1_sq + sigma2_sq + C2
-    cs = torch.mean(v1 / v2)
-
-    ssim_map = ((2 * mu1_mu2 + C1) * v1) / ((mu1_sq + mu2_sq + C1) * v2)
-
-    if size_average:
-        return ssim_map.mean(), cs
-    else:
-        return ssim_map.mean(1).mean(1).mean(1), cs
