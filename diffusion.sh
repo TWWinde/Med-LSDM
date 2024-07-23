5e9fcef64effb7c0c85aac63fdab36e2a6e1a5de
TWWinde <twwinde@gmail.com>
2024-07-23 16:23:58 +0200

    Update diffusion.sh

--- diffusion.sh
+++ diffusion.sh
@@ -10,11 +10,11 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-#SBATCH --gpus=rtx_a5000:1
-#SBATCH --nodelist=linse19
-# SBATCH --nodelist=linse23
-# SBATCH --qos=shortbatch
-# SBATCH --partition=highperf
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --nodelist=linse19
+#SBATCH --nodelist=linse23
+#SBATCH --qos=shortbatch
+#SBATCH --partition=highperf
 
 
 # Activate everything you need
@@ -62,7 +62,7 @@ python -c "import torch; print(torch.__version__)"
 
 # diffusion with vg_gan_SPADE
 python train/train_ddpm.py model=ddpm dataset=autopet model.results_folder_postfix='output_with_vq_gan_spade' dataset.label_nc=37 \
-model.vqgan_spade_ckpt = "/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan_spade/AutoPET/vq_gan_spade/lightning_logs/version_137077/checkpoints/latest_checkpoint.ckpt" \
+model.vqgan_spade_ckpt='/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan_spade/AutoPET/vq_gan_spade/lightning_logs/version_137077/checkpoints/latest_checkpoint.ckpt' \
 model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
 model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0
 
