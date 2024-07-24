e929f7a8415c2ca4f19f24391b1237116cef61b5
TWWinde <twwinde@gmail.com>
2024-07-24 12:59:47 +0200

    Update diffusion.sh

--- diffusion.sh
+++ diffusion.sh
@@ -63,7 +63,7 @@ python -c "import torch; print(torch.__version__)"
 # diffusion with segconv 64 condition
 python train/train_ddpm.py model=ddpm dataset=autopet model.results_folder_postfix='output_with_segconv_64out' dataset.label_nc=37 \
 model.vqgan_ckpt='/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/AutoPET1/flair/lightning_logs/version_133784/checkpoints/latest_checkpoint.ckpt' \
-model.seggan_ckpt=0 \
+model.seggan_ckpt=0 model.spade_input_channel=64 \
 model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
 model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1
 
