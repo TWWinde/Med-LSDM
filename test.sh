a8437c37d0b11d3377508b827e33f8465a1ceead
TWWinde <twwinde@gmail.com>
2024-08-05 21:04:24 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -49,7 +49,7 @@ python -c "import torch; print(torch.__version__)"
 #model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
 #model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1
 
-# autopet
+# vq_gan autopet
 PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=autopet \
 model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \  #8
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
