4184e6e96886d0ec2f99cc739dee1135bf0d0a2d
TWWinde <twwinde@gmail.com>
2024-08-19 22:30:04 +0200

    Update test_ddpm.py

--- test/test_ddpm.py
+++ test/test_ddpm.py
@@ -34,7 +34,8 @@ def inference(cfg: DictConfig):
             dim_mults=cfg.model.dim_mults,
             channels=cfg.model.diffusion_num_channels,
             label_nc=cfg.model.spade_input_channel if cfg.model.segconv == 1 else cfg.dataset.label_nc,
-            segconv=cfg.model.segconv
+            segconv=cfg.model.segconv,
+            vqvae=None if cfg.model.vqvae_ckpt == 0 else cfg.model.vqvae_ckpt
         ).cuda()
     elif cfg.model.denoising_fn == 'UNet':
         unet_model = UNet(
