1555588dbd2f53df9afb7f44586a683df7b3ce63
TWWinde <twwinde@gmail.com>
2024-08-31 16:23:12 +0200

    Update data_process.py

--- dataset/data_process.py
+++ dataset/data_process.py
@@ -614,9 +614,9 @@ def combine_label_duke(in_path, out_path, item):
 
     combined_label_sitk = sitk.GetImageFromArray(combined_label)
 
-    combined_label_sitk.SetSpacing(seg_vessels.GetSpacing())
-    combined_label_sitk.SetOrigin(seg_vessels.GetOrigin())
-    combined_label_sitk.SetDirection(seg_vessels.GetDirection())
+    #combined_label_sitk.SetSpacing(seg_vessels.GetSpacing())
+    #combined_label_sitk.SetOrigin(seg_vessels.GetOrigin())
+    #combined_label_sitk.SetDirection(seg_vessels.GetDirection())
 
     return combined_label_sitk.GetSize()
 
