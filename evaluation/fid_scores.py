16d967cdf10797dc35ef1d9bfc58cf874a35ec62
TWWinde <twwinde@gmail.com>
2024-08-31 17:43:53 +0200

    add 3D FID

--- evaluation/fid_scores.py
+++ evaluation/fid_scores.py
@@ -1,200 +1,237 @@
+
+
+# ---------------------------------------------------------------------------------------------#
+# This code is an adapted version of https://github.com/batmanlab/HA-GAN/tree/master/evaluation
+# ---------------------------------------------------------------------------------------------#
+
+
 import os
+import time
+from argparse import ArgumentParser
+from collections import OrderedDict
+
 import numpy as np
+from scipy import linalg
+
 import torch
-from scipy import linalg # For numpy FID
-from pathlib import Path
-from evaluation.fid_folder.inception import InceptionV3
-import matplotlib.pyplot as plt
-
-# --------------------------------------------------------------------------#
-# This code is an adapted version of https://github.com/mseitzer/pytorch-fid
-# --------------------------------------------------------------------------#
+import torch.nn as nn
 
 
-class fid_pytorch():
-    def __init__(self, opt, dataloader_val, dataloader_real=None, cross_domain_training=False):
-        self.opt = opt
-        self.dims = 2048
-        block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[self.dims]
-        self.model_inc = InceptionV3([block_idx])
-        if opt.gpu_ids != "-1":
-            self.model_inc.cuda()
-        self.val_dataloader = dataloader_val
-        self.dataloader_real = dataloader_real
-        self.cross_domain_training = cross_domain_training
-        if self.cross_domain_training:
-            self.m1, self.s1 = self.compute_statistics_of_val_path(dataloader_real)
-        else:
-            self.m1, self.s1 = self.compute_statistics_of_val_path(dataloader_val)
-        self.best_fid = 99999999
-        self.path_to_save = os.path.join(self.opt.checkpoints_dir, self.opt.name, "FID")
-        Path(self.path_to_save).mkdir(parents=True, exist_ok=True)
+from evaluation.resnet3D import resnet50
 
-    def compute_statistics_of_val_path(self, dataloader_val):
-        print("--- Now computing Inception activations for real set ---")
-        pool = self.accumulate_inception_activations(dataloader_val)
-        mu, sigma = torch.mean(pool, 0), torch_cov(pool, rowvar=False)
-        print("--- Finished FID stats for real set ---")
-        return mu, sigma
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+torch.backends.cudnn.benchmark = True
 
-    def accumulate_inception_activations(self, dataloader_val):
-        pool, logits, labels = [], [], []
-        self.model_inc.eval()
+parser = ArgumentParser()
+parser.add_argument('--path', type=str, default='')
+parser.add_argument('--real_suffix', type=str, default='eval_600_size_256_resnet50_fold')
+parser.add_argument('--img_size', type=int, default=256)
+parser.add_argument('--batch_size', type=int, default=2)
+parser.add_argument('--num_workers', type=int, default=8)
+parser.add_argument('--num_samples', type=int, default=2048)
+parser.add_argument('--dims', type=int, default=2048)
+parser.add_argument('--ckpt_step', type=int, default=80000)
+parser.add_argument('--latent_dim', type=int, default=1024)
+parser.add_argument('--basename', type=str, default="256_1024_Alpha_SN_v4plus_4_l1_GN_threshold_600_fold")
+parser.add_argument('--fold', type=int)
+
+def trim_state_dict_name(ckpt):
+    new_state_dict = OrderedDict()
+    for k, v in ckpt.items():
+        name = k[7:] # remove `module.`
+        new_state_dict[name] = v
+    return new_state_dict
+
+class Flatten(torch.nn.Module):
+    def forward(self, inp):
+        return inp.view(inp.size(0), -1)
+
+def generate_samples(args):
+    G = Generator(mode='eval', latent_dim=args.latent_dim, num_class=0)
+    ckpt_path = "./checkpoint/"+args.basename+str(args.fold)+"/G_iter"+str(args.ckpt_step)+".pth"
+    ckpt = torch.load(ckpt_path)['model']
+    ckpt = trim_state_dict_name(ckpt)
+    G.load_state_dict(ckpt)
+
+    E = Encoder()
+    ckpt_path = "./checkpoint/"+args.basename+str(args.fold)+"/E_iter"+str(args.ckpt_step)+".pth"
+    ckpt = torch.load(ckpt_path)['model']
+    ckpt = trim_state_dict_name(ckpt)
+    E.load_state_dict(ckpt)
+
+    Sub_E = Sub_Encoder(args.latent_dim=args.latent_dim)
+    ckpt_path = "./checkpoint/"+args.basename+str(args.fold)+"/Sub_E_iter"+str(args.ckpt_step)+".pth"
+    ckpt = torch.load(ckpt_path)['model']
+    ckpt = trim_state_dict_name(ckpt)
+    Sub_E.load_state_dict(ckpt)
+    print("Weights step", args.ckpt_step, "loaded.")
+    del ckpt
+
+    G = nn.DataParallel(G).cuda()
+    E = nn.DataParallel(E).cuda()
+    Sub_E = nn.DataParallel(Sub_E).cuda()
+
+    G.eval()
+    E.eval()
+    Sub_E.eval()
+
+
+    model = get_feature_extractor()
+    pred_arr = np.empty((args.num_samples, args.dims))
+
+    for i in range(args.num_samples//args.batch_size):
+        if i % 10 == 0:
+            print('\rPropagating batch %d' % i, end='', flush=True)
         with torch.no_grad():
-            for i, data_i in enumerate(dataloader_val):
-                image = data_i["image"]
-                if self.opt.gpu_ids != "-1":
-                    image = image.cuda()
-                image = (image + 1) / 2
-                #image = torch.nn.functional.interpolate(image,scale_factor = 0.5,mode ='nearest')
-                pool_val = self.model_inc(image.float())[0][:, :, 0, 0]
-                pool += [pool_val]
-        return torch.cat(pool, 0)
 
-    def compute_fid_with_valid_path(self, model):
-        pool, logits, labels = [], [], []
-        self.model_inc.eval()
+            noise = torch.randn((args.batch_size, args.latent_size)).cuda()
+            x_rand = G(noise) # dumb index 0, not used
+            # range: [-1,1]
+            x_rand = x_rand.detach()
+            pred = model(x_rand)
 
-        with torch.no_grad():
-            for i, data_i in enumerate(self.val_dataloader):
-                image, label = models.preprocess_input(self.opt, data_i, test=False)
-                if self.opt.no_EMA:
-                    generated = netG(label)  #### should delete edge
-                else:
-                    generated = netEMA(label)
-                generated = (generated + 1) / 2
-                pool_val = self.model_inc(generated.float())[0][:, :, 0, 0]
-                pool += [pool_val]
-            pool = torch.cat(pool, 0)
-            mu, sigma = torch.mean(pool, 0), torch_cov(pool, rowvar=False)
-            answer = self.numpy_calculate_frechet_distance(self.m1, self.s1, mu, sigma)
-        netG.train()
-        if not self.opt.no_EMA:
-            netEMA.train()
-        return answer
-
-    def numpy_calculate_frechet_distance(self, mu1, sigma1, mu2, sigma2, eps=1e-6):
-        """Numpy implementation of the Frechet Distance.
-        Taken from https://github.com/bioinf-jku/TTUR
-        The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
-        and X_2 ~ N(mu_2, C_2) is
-                d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
-        Stable version by Dougal J. Sutherland.
-        Params:
-        -- mu1   : Numpy array containing the activations of a layer of the
-                   inception net (like returned by the function 'get_predictions')
-                   for generated samples.
-        -- mu2   : The sample mean over activations, precalculated on an
-                   representive data set.
-        -- sigma1: The covariance matrix over activations for generated samples.
-        -- sigma2: The covariance matrix over activations, precalculated on an
-                   representive data set.
-        Returns:
-        --   : The Frechet Distance.
-        """
-
-        mu1, sigma1, mu2, sigma2 = mu1.detach().cpu().numpy(), sigma1.detach().cpu().numpy(), mu2.detach().cpu().numpy(), sigma2.detach().cpu().numpy()
-
-        mu1 = np.atleast_1d(mu1)
-        mu2 = np.atleast_1d(mu2)
-
-        sigma1 = np.atleast_2d(sigma1)
-        sigma2 = np.atleast_2d(sigma2)
-
-        assert mu1.shape == mu2.shape, \
-            'Training and test mean vectors have different lengths'
-        assert sigma1.shape == sigma2.shape, \
-            'Training and test covariances have different dimensions'
-
-        diff = mu1 - mu2
-
-        # Product might be almost singular
-        covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
-        if not np.isfinite(covmean).all():
-            msg = ('fid calculation produces singular product; '
-                   'adding %s to diagonal of cov estimates') % eps
-            print(msg)
-            offset = np.eye(sigma1.shape[0]) * eps
-            covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
-
-        # Numerical error might give slight imaginary component
-        if np.iscomplexobj(covmean):
-            #print('wat')
-            if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
-                m = np.max(np.abs(covmean.imag))
-                #print('Imaginary component {}'.format(m))
-            covmean = covmean.real
-
-        tr_covmean = np.trace(covmean)
-
-        out = diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
-        return out
-
-    def update(self, model, cur_iter):
-        print("--- Iter %s: computing FID ---" % (cur_iter))
-        cur_fid = self.compute_fid_with_valid_path(model.module.netG, model.module.netEMA,model)
-        self.update_logs(cur_fid, cur_iter)
-        print("--- FID at Iter %s: " % cur_iter, "{:.2f}".format(cur_fid))
-        if cur_fid < self.best_fid:
-            self.best_fid = cur_fid
-            is_best = True
+        if (i+1)*args.batch_size > pred_arr.shape[0]:
+            pred_arr[i*args.batch_size:] = pred.cpu().numpy()
         else:
-            is_best = False
-        return is_best
+            pred_arr[i*args.batch_size:(i+1)*args.batch_size] = pred.cpu().numpy()
 
-    def update_logs(self, cur_fid, epoch):
-        try :
-            np_file = np.load(self.path_to_save + "/fid_log.npy")
-            first = list(np_file[0, :])
-            sercon = list(np_file[1, :])
-            first.append(epoch)
-            sercon.append(cur_fid)
-            np_file = [first, sercon]
-        except:
-            np_file = [[epoch], [cur_fid]]
+    print(' done')
+    return pred_arr
 
-        np.save(self.path_to_save + "/fid_log.npy", np_file)
+def get_activations_from_dataloader(model, data_loader, args):
 
-        np_file = np.array(np_file)
-        plt.figure()
-        plt.plot(np_file[0, :], np_file[1, :])
-        plt.grid(b=True, which='major', color='#666666', linestyle='--')
-        plt.minorticks_on()
-        plt.grid(b=True, which='minor', color='#999999', linestyle='--', alpha=0.2)
-        plt.savefig(self.path_to_save + "/plot_fid", dpi=600)
-        plt.close()
+    pred_arr = np.empty((args.num_samples, args.dims))
+    for i, batch in enumerate(data_loader):
+        if i % 10 == 0:
+            print('\rPropagating batch %d' % i, end='', flush=True)
+        batch = batch.float().cuda()
+        with torch.no_grad():
+            pred = model(batch)
 
-    def fid_test(self, model):
-        print("--- test: computing FID ---")
-        cur_fid = self.compute_fid_with_valid_path(model.module.netG, model.module.netEMA,model)
-        print("--- FID at test : ", "{:.2f}".format(cur_fid))
+        if i*args.batch_size > pred_arr.shape[0]:
+            pred_arr[i*args.batch_size:] = pred.cpu().numpy()
+        else:
+            pred_arr[i*args.batch_size:(i+1)*args.batch_size] = pred.cpu().numpy()
+    print(' done')
+    return pred_arr
 
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
 
-def torch_cov(m, rowvar=False):
-    '''Estimate a covariance matrix given data.
-    Covariance indicates the level to which two variables vary together.
-    If we examine N-dimensional samples, `X = [x_1, x_2, ... x_N]^T`,
-    then the covariance matrix element `C_{ij}` is the covariance of
-    `x_i` and `x_j`. The element `C_{ii}` is the variance of `x_i`.
-    Args:
-        m: A 1-D or 2-D array containing multiple variables and observations.
-            Each row of `m` represents a variable, and each column a single
-            observation of all those variables.
-        rowvar: If `rowvar` is True, then each row represents a
-            variable, with observations in the columns. Otherwise, the
-            relationship is transposed: each column represents a variable,
-            while the rows contain observations.
+    Stable version by Dougal J. Sutherland.
+
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+
     Returns:
-        The covariance matrix of the variables.
-    '''
-    if m.dim() > 2:
-        raise ValueError('m has more than 2 dimensions')
-    if m.dim() < 2:
-        m = m.view(1, -1)
-    if not rowvar and m.size(0) != 1:
-        m = m.t()
-    # m = m.type(torch.double)  # uncomment this line if desired
-    fact = 1.0 / (m.size(1) - 1)
-    m -= torch.mean(m, dim=1, keepdim=True)
-    mt = m.t()  # if complex: mt = m.t().conj()
-    return fact * m.matmul(mt).squeeze()
+    --   : The Frechet Distance.
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) +
+            np.trace(sigma2) - 2 * tr_covmean)
+
+def post_process(act):
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
+def get_feature_extractor():
+    model = resnet50(shortcut_type='B')
+    model.conv_seg = nn.Sequential(nn.AdaptiveAvgPool3d((1, 1, 1)),
+                                   Flatten()) # (N, 512)
+    # ckpt from https://drive.google.com/file/d/1399AsrYpQDi1vq6ciKRQkfknLsQQyigM/view?usp=sharing
+    ckpt = torch.load("../gnn_shared/ckpt/pretrain/resnet_50.pth")
+    ckpt = trim_state_dict_name(ckpt["state_dict"])
+    model.load_state_dict(ckpt) # No conv_seg module in ckpt
+    model = nn.DataParallel(model).cuda()
+    model.eval()
+    print("Feature extractor weights loaded")
+    return model
+
+def calculate_fid_real(args):
+    """Calculates the FID of two paths"""
+    assert os.path.exists("./results/fid/m_real_"+args.real_suffix+str(args.fold)+".npy")
+
+    model = get_feature_extractor()
+    #dataset = COPD_dataset(img_size=args.img_size, stage="train", fold=args.fold, threshold=600)
+    dataset = Brain_dataset(img_size=args.img_size, stage="train", fold=args.fold)
+    args.num_samples = len(dataset)
+    print("Number of samples:", args.num_samples)
+    data_loader = torch.utils.data.DataLoader(dataset,batch_size=args.batch_size,drop_last=False,
+                                               shuffle=False,num_workers=args.num_workers)
+    act = get_activations_from_dataloader(model, data_loader, args)
+    np.save("./results/fid/pred_arr_real_train_size_"+str(args.img_size)+"_resnet50_GSP_fold"+str(args.fold)+".npy", act)
+    #np.save("./results/fid/pred_arr_real_train_600_size_"+str(args.img_size)+"_resnet50_fold"+str(args.fold)+".npy", act)
+    #calculate_mmd(args, act)
+    m, s = post_process(act)
+
+    m1 = np.load("./results/fid/m_real_"+args.real_suffix+str(args.fold)+".npy")
+    s1 = np.load("./results/fid/s_real_"+args.real_suffix+str(args.fold)+".npy")
+
+    fid_value = calculate_frechet_distance(m1, s1, m, s)
+    print('FID: ', fid_value)
+    #np.save("./results/fid/m_real_train_600_size_"+str(args.img_size)+"_resnet50_fold"+str(args.fold)+".npy", m)
+    #np.save("./results/fid/s_real_train_600_size_"+str(args.img_size)+"_resnet50_fold"+str(args.fold)+".npy", s)
+    #np.save("./results/fid/m_real_train_size_"+str(args.img_size)+"_resnet50_GSP_fold"+str(args.fold)+".npy", m)
+    #np.save("./results/fid/s_real_train_size_"+str(args.img_size)+"_resnet50_GSP_fold"+str(args.fold)+".npy", s)
+
+def calculate_fid_fake(args):
+    #assert os.path.exists("./results/fid/m_real_"+args.real_suffix+str(args.fold)+".npy")
+    act = generate_samples(args)
+    m2, s2 = post_process(act)
+
+    m1 = np.load("./results/fid/m_real_"+args.real_suffix+str(args.fold)+".npy")
+    s1 = np.load("./results/fid/s_real_"+args.real_suffix+str(args.fold)+".npy")
+
+    fid_value = calculate_frechet_distance(m1, s1, m2, s2)
+    print('FID: ', fid_value)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    start_time = time.time()
+    calculate_fid_real(args)
+    calculate_fid_fake(args)
+    print("Done. Using", (time.time()-start_time)//60, "minutes.")
