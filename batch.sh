b65872cbaab3e0da69128eeb6c58a61d57abe821
TWWinde <twwinde@gmail.com>
2024-09-15 19:03:39 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -102,12 +102,12 @@ export CUDA_LAUNCH_BLOCKING=1
 #model.gan_feat_weight=4 model.batch_size=4 model.n_codes=16384  \
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real'
 
-# duke_only_t1_all
+# duke_t1_all
 PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
-model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_only_t1_all' model.precision=32 model.embedding_dim=8 \
+model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_all' model.precision=32 model.embedding_dim=8 \
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384  \
+model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
 dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop'
 
 # semantic map
