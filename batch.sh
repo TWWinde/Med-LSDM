f7709875fb9187a5ed3715059a305888d818ad0a
TWWinde <twwinde@gmail.com>
2024-09-15 18:57:11 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -103,8 +103,6 @@ export CUDA_LAUNCH_BLOCKING=1
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real'
 
 # duke_only_t1_all
-python /no_backups/d1502/medicaldiffusion/dataset/data_process.py
-
 PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
 model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_only_t1_all' model.precision=32 model.embedding_dim=8 \
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
