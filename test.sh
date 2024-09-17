f99c152315162832327ec7a26202e9b6d678a102
TWWinde <twwinde@gmail.com>
2024-09-17 18:04:22 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,12 +9,12 @@
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --qos=batch
-#SBATCH --gpus=rtx_a5000:1
-#SBATCH --nodelist=linse19
-# SBATCH --nodelist=linse23
-# SBATCH --qos=shortbatch
-# SBATCH --partition=highperf
+# SBATCH --qos=batch
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --nodelist=linse19
+#SBATCH --nodelist=linse23
+#SBATCH --qos=shortbatch
+#SBATCH --partition=highperf
 
 
 # Activate everything you need
