8551875ccc593562b6b703fc4463a0e91b03eaa5
TWWinde <twwinde@gmail.com>
2024-09-18 17:26:38 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -10,9 +10,9 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-#SBATCH --nodelist=linse21
-#SBATCH --qos=shortbatch
-#SBATCH --partition=highperf
+# SBATCH --nodelist=linse21
+# SBATCH --qos=shortbatch
+# SBATCH --partition=highperf
 # SBATCH --gpus=rtx_a5000:1
 # SBATCH --nodelist=linse19
 
