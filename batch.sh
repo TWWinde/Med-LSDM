baa826935e9c0951c50aecab65fd8f4ead3cdb89
TWWinde <twwinde@gmail.com>
2024-09-18 22:05:25 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -13,8 +13,8 @@
 # SBATCH --nodelist=linse21
 # SBATCH --qos=shortbatch
 # SBATCH --partition=highperf
-#SBATCH --gpus=rtx_a5000:1
-#SBATCH --nodelist=linse19
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --nodelist=linse19
 
 
 # Activate everything you need
@@ -101,6 +101,14 @@ model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_c
 model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
 dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all'
+
+# autopet_tanh
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model.precision=16 model.embedding_dim=8 \  #8
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
 
 # duke_t1_all
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
