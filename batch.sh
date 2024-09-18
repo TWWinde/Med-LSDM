b0af80d679f34aabe0405f2e75272db0a23e0e54
TWWinde <twwinde@gmail.com>
2024-09-18 17:18:45 +0200

    test

--- batch.sh
+++ batch.sh
@@ -10,9 +10,9 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-# SBATCH --nodelist=linse21
-# SBATCH --qos=shortbatch
-# SBATCH --partition=highperf
+#SBATCH --nodelist=linse21
+#SBATCH --qos=shortbatch
+#SBATCH --partition=highperf
 # SBATCH --gpus=rtx_a5000:1
 # SBATCH --nodelist=linse19
 
@@ -49,7 +49,7 @@ PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad20
 model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='mr_final' model.precision=16 model.embedding_dim=8 \
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
+model.gan_feat_weight=4 model.batch_size=4 model.n_codes=16384
 
 # mr_wo_mask
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023_wo_mask \
