c6b8bbd5d31c4b4ceb90554e1ac6814561bafaf4
TWWinde <twwinde@gmail.com>
2024-09-18 22:11:12 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -104,7 +104,7 @@ export CUDA_LAUNCH_BLOCKING=1
 
 # autopet_tanh
 PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
-model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model.precision=16 model.embedding_dim=8 \  #8
+model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model.precision=16 model.embedding_dim=8 \
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
