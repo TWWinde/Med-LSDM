3f0b67e89f4ac00bd16c66c64e18df53e298d6f4
TWWinde <twwinde@gmail.com>
2024-09-18 22:13:18 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=VQ-GAN%j.%N.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=1-23:00:00
+#SBATCH --time=3-23:00:00
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --gpus=1
@@ -108,7 +108,6 @@ model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
-
 
 # duke_t1_all
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
