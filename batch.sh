137d2abb0eb998a9c7d5d62ddf436c1d8bb2fe5b
TWWinde <twwinde@gmail.com>
2024-09-21 21:06:46 +0200

    update

--- batch.sh
+++ batch.sh
@@ -95,12 +95,12 @@ export CUDA_LAUNCH_BLOCKING=1
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
 
 # duke_only_t1_randomcrop_tanh
-#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
-#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_randomcrop_t1_tanh' model.precision=16 model.embedding_dim=8 \
-#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
-#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all'
+PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_randomcrop_t1_real_tanh' model.precision=16 model.embedding_dim=8 \
+model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real'
 
 # autopet_tanh
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
@@ -126,12 +126,12 @@ export CUDA_LAUNCH_BLOCKING=1
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop'
 
 # duke_t1_tanh
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
-model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_real_tanh' model.precision=32 model.embedding_dim=8 \
-model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
-dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_real_tanh' model.precision=32 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
 
 # semantic map
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=semanticmap \
