b8adabf63b82d86801c32e2eb038e8a66a3ed5e1
TWWinde <twwinde@gmail.com>
2024-09-27 22:33:10 +0200

    Update test_vq_gan.sh

--- test_vq_gan.sh
+++ test_vq_gan.sh
@@ -109,7 +109,7 @@ python -c "import torch; print(torch.__version__)"
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop'
 
 # duke_t1_tanh
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+PL_TORCH_DISTRIBUTED_BACKEND=gloo python /misc/no_backups/d1502/medicaldiffusion/test/test_vqgan.py dataset=duke \
 model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_real_tanh' model.precision=32 model.embedding_dim=8 \
 model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/DUKE/results_t1_real_tanh/lightning_logs/version_144592/checkpoints/latest_checkpoint.ckpt" \
 model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
