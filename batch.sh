fdbe2d1b69f7644c6feb42d5e07324a7966a68d0
TWWinde <twwinde@gmail.com>
2024-09-29 00:30:08 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -126,12 +126,12 @@ export CUDA_LAUNCH_BLOCKING=1
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop'
 
 # duke_t1_tanh
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
-model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_real_tanh__' model.precision=32 model.embedding_dim=8 \
-model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
-dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_real_tanh' model.precision=32 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
 
 # semantic map
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=semanticmap \
@@ -167,9 +167,9 @@ dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' datas
 #model.batch_size=2 model.n_codes=16384
 
 # vq_gan_spade duke
-#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
-#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_duke' model.precision=16 model.embedding_dim=8 \
-#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
+PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_duke' model.precision=16 model.embedding_dim=8 \
+model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
 
