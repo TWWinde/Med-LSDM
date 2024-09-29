a24548d7880858fc8590b07ffeb9fbfdf7c2fd39
TWWinde <twwinde@gmail.com>
2024-09-29 17:20:44 +0200

    test

--- test_diffusion.sh
+++ test_diffusion.sh
@@ -2,7 +2,7 @@
 
 #Slurm parameters
 #SBATCH --job-name=test_diffusion
-#SBATCH --output=test%j.%N.out
+#SBATCH --output=test_diff%j.%N.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=4-23:00:00
@@ -50,12 +50,12 @@ python -c "import torch; print(torch.__version__)"
 #model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1
 
 # diffusion with segconv 64 condition and vq_spade
-#python /misc/no_backups/d1502/medicaldiffusion/test/test_ddpm.py  model=ddpm dataset=autopet model.results_folder_postfix='output_with_vq_spade_segconv_64out' dataset.label_nc=37 \
-#model.vqgan_ckpt=0 \
-#model.vqgan_spade_ckpt="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan_spade/AutoPET/vq_gan_spade/lightning_logs/version_137357/checkpoints/latest_checkpoint.ckpt" \
-#model.vqvae_ckpt=0 model.spade_input_channel=64 \
-#model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
-#model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1 model.load_milestone=0
+python /misc/no_backups/d1502/medicaldiffusion/test/test_ddpm.py  model=ddpm dataset=autopet model.results_folder_postfix='output_with_vq_spade_segconv_64out' dataset.label_nc=37 \
+model.vqgan_ckpt=0 \
+model.vqgan_spade_ckpt="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan_spade/AutoPET/vq_gan_spade/lightning_logs/version_137357/checkpoints/latest_checkpoint.ckpt" \
+model.vqvae_ckpt=0 model.spade_input_channel=64 \
+model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
+model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1 model.load_milestone=0
 
 
 # diffusion with vqvae condition autopet
@@ -98,10 +98,10 @@ python -c "import torch; print(torch.__version__)"
 #dataset.root_dir='/data/private/autoPET/duke/final_labeled_mr' dataset.val_dir='/data/private/autoPET/duke/final_labeled_mr'
 
 # diffusion with segconv 3 condition duke
-python /misc/no_backups/d1502/medicaldiffusion/test/test_ddpm.py model=ddpm dataset=duke model.results_folder_postfix='results_duke_final_3' dataset.label_nc=3 \
-model.vqgan_ckpt='/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/DUKE/results_t1_all_tanh/lightning_logs/version_144222/checkpoints/latest_checkpoint.ckpt' \
-model.vqvae_ckpt=0 model.spade_input_channel=3 \
-model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
-model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=2 model.gpus=0 model.segconv=1 model.load_milestone=0 \
-dataset.root_dir='/data/private/autoPET/duke/final_labeled_mr' dataset.val_dir='/data/private/autoPET/duke/final_labeled_mr'
+#python /misc/no_backups/d1502/medicaldiffusion/test/test_ddpm.py model=ddpm dataset=duke model.results_folder_postfix='results_duke_final_3' dataset.label_nc=3 \
+#model.vqgan_ckpt='/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/DUKE/results_t1_all_tanh/lightning_logs/version_144222/checkpoints/latest_checkpoint.ckpt' \
+#model.vqvae_ckpt=0 model.spade_input_channel=3 \
+#model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
+#model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=2 model.gpus=0 model.segconv=1 model.load_milestone=0 \
+#dataset.root_dir='/data/private/autoPET/duke/final_labeled_mr' dataset.val_dir='/data/private/autoPET/duke/final_labeled_mr'
 
