4907d5151424508194d2730865fb157288817a26
TWWinde <twwinde@gmail.com>
2024-09-29 17:53:49 +0200

    Update test_vq_gan.sh

--- test_vq_gan.sh
+++ test_vq_gan.sh
@@ -10,8 +10,8 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-#SBATCH --gpus=rtx_a5000:1
-#SBATCH --nodelist=linse19
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --nodelist=linse19
 # SBATCH --nodelist=linse21
 # SBATCH --qos=shortbatch
 # SBATCH --partition=highperf
@@ -30,39 +30,39 @@ python -c "import torch; print(torch.__version__)"
 
 # vq_gan autopet
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=autopet \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/AutoPET/results/lightning_logs/version_133784/checkpoints/latest_checkpoint.ckpt" \
-#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/AutoPET/results/lightning_logs/version_133784/checkpoints/latest_checkpoint.ckpt" \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_autopet' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 #model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # vq_gan_spade autopet
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=autopet \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan_spade/AutoPET/vq_gan_spade/lightning_logs/version_137357/checkpoints/latest_checkpoint.ckpt" \
-#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade' model.precision=16 model.embedding_dim=8 \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan_spade/AutoPET/vq_gan_spade/lightning_logs/version_137357/checkpoints/latest_checkpoint.ckpt" \
+#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_autopet' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 #model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
 
 # vq_gan mr
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=synthrad2023 \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/SynthRAD2023/results/lightning_logs/version_133718/checkpoints/latest_checkpoint.ckpt" \
-#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/SynthRAD2023/results/lightning_logs/version_133718/checkpoints/latest_checkpoint.ckpt" \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_vqgan_mr' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 #model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # vq_gan_spade mr
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=synthrad2023 \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan_spade/SynthRAD2023/vq_gan_spade/lightning_logs/version_137360/checkpoints/latest_checkpoint.ckpt" \
-#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan_spade/SynthRAD2023/vq_gan_spade/lightning_logs/version_137360/checkpoints/latest_checkpoint.ckpt" \
+#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='results_vq_gan_spade_mr' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 #model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # vq_gan mr totalsegmentator
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=totalsegmentator_mri \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/TotalSegmentator_mri/results/lightning_logs/version_137427/checkpoints/latest_checkpoint.ckpt" \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/TotalSegmentator_mri/results/lightning_logs/version_137427/checkpoints/latest_checkpoint.ckpt" \
 #model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
@@ -70,7 +70,7 @@ python -c "import torch; print(torch.__version__)"
 
 # vq_gan mr totalsegmentator fine tuned with mr
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=totalsegmentator_mri \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_gan/TotalSegmentator_mri/results/lightning_logs/version_139199/checkpoints/latest_checkpoint.ckpt" \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/TotalSegmentator_mri/results/lightning_logs/version_139199/checkpoints/latest_checkpoint.ckpt" \
 #model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
@@ -78,7 +78,7 @@ python -c "import torch; print(torch.__version__)"
 
 # vq_vae synthrad mr
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python test/test_vqgan.py dataset=synthrad2023 \
-#model.resume_from_checkpoint="/misc/no_backups/d1502/medicaldiffusion/checkpoints/vq_vae/SynthRAD2023/results/lightning_logs/version_139999/checkpoints/latest_checkpoint.ckpt" \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_vae/SynthRAD2023/results/lightning_logs/version_139999/checkpoints/latest_checkpoint.ckpt" \
 #model=vq_vae model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.batch_size=2 model.n_codes=16384
@@ -118,12 +118,12 @@ python -c "import torch; print(torch.__version__)"
 #dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
 
 # autopet_tanh
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python /misc/no_backups/d1502/medicaldiffusion/test/test_vqgan.py dataset=autopet \
-model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model.precision=16 model.embedding_dim=8 \
-model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/AutoPET/autopet_tanh/lightning_logs/version_144368/checkpoints/latest_checkpoint.ckpt" \
-model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
-model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
-model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python /misc/no_backups/d1502/medicaldiffusion/test/test_vqgan.py dataset=autopet \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model.precision=16 model.embedding_dim=8 \
+#model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/AutoPET/autopet_tanh/lightning_logs/version_144368/checkpoints/latest_checkpoint.ckpt" \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 
 # synthrad_tanh
 #PL_TORCH_DISTRIBUTED_BACKEND=gloo python /misc/no_backups/d1502/medicaldiffusion/test/test_vqgan.py dataset=synthrad2023 \
@@ -132,3 +132,11 @@ model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
 #model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
 #model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
 #model.gan_feat_weight=4 model.batch_size=4 model.n_codes=16384
+
+# vq_gan_spade duke
+PL_TORCH_DISTRIBUTED_BACKEND=gloo python /misc/no_backups/d1502/medicaldiffusion/test/test_vqgan.py dataset=duke \
+model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_duke' model.precision=16 model.embedding_dim=8 \
+model.resume_from_checkpoint="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan_spade/DUKE/vq_gan_spade_tanh_duke/lightning_logs/version_145400/checkpoints/latest_checkpoint.ckpt" \
+model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=1e-5 \
+model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
