4611a4c15b783c477a8883d0ff4c34a2b6a6e4e4
TWWinde <twwinde@gmail.com>
2024-10-05 13:48:45 +0200

    Update train_unet.py

--- train/train_unet.py
+++ train/train_unet.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+sys.path.append('/misc/no_backups/d1502/medicaldiffusion')
 import os
 from collections import OrderedDict
 import numpy as np
