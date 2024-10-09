b3800bbaa21ac476cc44dc55ef95eecd7b0bb0f6
TWWinde <twwinde@gmail.com>
2024-10-09 17:36:32 +0200

    update

--- test_diffusion.sh
+++ test_diffusion.sh
@@ -107,11 +107,17 @@ python -c "import torch; print(torch.__version__)"
 #model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=2 model.gpus=0 model.segconv=1 model.load_milestone=0
 
 # diffusion with segconv 8 condition vq_gan_spade duke
-python /misc/no_backups/d1502/medicaldiffusion/test/test_ddpm.py model=ddpm dataset=duke model.results_folder_postfix='results_duke_vq_spade_segconv_8out' dataset.label_nc=3 \
-model.vqgan_ckpt=0 \
-model.vqgan_spade_ckpt="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan_spade/DUKE/vq_gan_spade_tanh_duke/lightning_logs/version_145400/checkpoints/latest_checkpoint.ckpt" \
-model.vqvae_ckpt=0 model.spade_input_channel=8 \
-model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
-model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1 model.load_milestone=0
+#python /misc/no_backups/d1502/medicaldiffusion/test/test_ddpm.py model=ddpm dataset=duke model.results_folder_postfix='results_duke_vq_spade_segconv_8out' dataset.label_nc=3 \
+#model.vqgan_ckpt=0 \
+#model.vqgan_spade_ckpt="/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan_spade/DUKE/vq_gan_spade_tanh_duke/lightning_logs/version_145400/checkpoints/latest_checkpoint.ckpt" \
+#model.vqvae_ckpt=0 model.spade_input_channel=8 \
+#model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
+#model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=1 model.gpus=0 model.segconv=1 model.load_milestone=0
 
+# diffusion synthrad with segconv 64 condition
+python train/train_ddpm.py model=ddpm dataset=synthrad2023 model.results_folder_postfix='output_mr_segconv_64_tanh' dataset.label_nc=31 \
+model.vqgan_ckpt='/data/private/autoPET/medicaldiffusion_results/results/checkpoints/vq_gan/SynthRAD2023/mr_final/lightning_logs/version_144347/checkpoints/latest_checkpoint.ckpt' \
+model.vqvae_ckpt=0 model.spade_input_channel=64 model.add_seg_to_noise=0 \
+model.diffusion_img_size=64 model.diffusion_depth_size=8 model.denoising_fn=Unet3D_SPADE model.diffusion=SemanticGaussianDiffusion \
+model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=2 model.gpus=0 model.segconv=1 model.load_milestone=0
 
