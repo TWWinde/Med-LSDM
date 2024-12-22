5f62a86654fb07a242bc7f4c9ecadf57b313cf52
TWWinde <st180408@stud.uni-stuttgart.de>
2024-12-22 14:59:48 +0100

    update readme

--- README.md
+++ README.md
@@ -1,62 +1,104 @@
 # 3D Semantic Medical Images Synthesis via Latent Diffusion 
 
-This repository contains the code to my Master Thesis
+In the medical domain, acquiring large datasets is challenging due to both accessibility issues and stringent privacy regulations. Consequently, data availability and privacy protection are major obstacles to applying machine learning in medical imaging. To address this, our study proposes the Med-LSDM (Latent Semantic Diffusion Model), which operates directly in the 3D domain and leverages de-identified semantic maps to generate synthetic data as a method of privacy preservation and data augmentation. Unlike many existing methods that focus on generating 2D slices, Med-LSDM is designed specifically for 3D semantic image synthesis, making it well-suited for applications requiring full volumetric data. Med-LSDM incorporates a guiding mechanism that controls the 3D image generation process by applying a diffusion model within the latent space of a pre-trained VQ-GAN. By operating in the compressed latent space, the model significantly reduces computational complexity while still preserving critical 3D spatial details. Our approach demonstrates strong performance in 3D semantic medical image synthesis, achieving a 3D-FID score of 0.0054 on the conditional Duke Breast dataset and similar Dice scores (0.70964) to those of real images (0.71496). These results prove the synthetic data from our model has small domain gap with real data and is useful for data augmentation.
 
+## Med-LSDM Framework
+we utilize Med-LSDM (Latent Semantic Diffusion Model) architecture for 3D Semantic Medical Image Synthesis. First, we present the whole
+architecture, then we introduce 3D VQ-GAN and Semantic Diffusion model in detail. To the
+best of our knowledge, it is the first work to generate 3D medical images from semantic maps
+using latent diffusion.
+![model.png](assets//model.png)
 
-
-# System Requirements
+## System Requirements
 This code has been tested on Ubuntu 20.04 and an NVIDIA A6000 GPU. Furthermore it was developed using Python v3.8.
 
-# Setup
+## Setup
+First, clone this repository:
+```
+git https://github.com/TWWinde/medicaldiffusion.git
+cd medicaldiffusion
+```
+
 In order to run our model, we suggest you create a virtual environment 
 ```
-conda create -n medicaldiffusion python=3.8
+conda create -n semanticmedicaldiffusion python=3.8
 ``` 
 and activate it with 
 ```
-conda activate medicaldiffusion
+conda activate semanticmedicaldiffusion
 ```
 Subsequently, download and install the required libraries by running 
 ```
 pip install -r requirements.txt
 ```
-
-# Training
-Once all libraries are installed and the datasets have been downloaded, you are ready to train the model:
-
-First, we need to train the three-dimensional VQ-GAN model. To do so in the BraTS dataset, you can run the following command:
+## Datasets
+We conduct our methods on three benchmark datasets: AutoPET, SynthRAD2023 and Duke Breast dataset.
+## Training
+First, we need to train the three-dimensional VQ-GAN model.  You can run the following command:
 
 ```
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=brats dataset.root_dir=<INSERT_PATH_TO_BRATS_DATASET> model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 model.n_hiddens=16 model.downsample=[2,2,2] model.num_workers=32 model.gradient_clip_val=1.0 model.lr=3e-4 model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384 model.accumulate_grad_batches=1 
+sbatch vqgan.sh
 ```
-Note that you need to provide the path to the dataset (e.g. ```dataset.root_dir='/data/BraTS/BraTS 2020'```) to successfully run the command.
+Note that you need to provide the path to the dataset (e.g. ```dataset.root_dir='/data/
+'```) to successfully run the command.
 
 To train the diffusion model in the latent space of the previously trained VQ-GAN model, you need to run the following command
 ```
-python train/train_ddpm.py model=ddpm dataset=brats model.results_folder_postfix='flair' model.vqgan_ckpt=<INSERT_PATH_TO_CHECKPOINT> model.diffusion_img_size=32 model.diffusion_depth_size=32 model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=10 model.gpus=1
+sbatch diffusion.sh
 ```
-Where you again need to specify the path to the VQ-GAN checkpoint from before (e.g. ```model.vqgan_ckpt='/home/<user>/Desktop/medicaldiffusion/checkpoints/vq_gan/BRATS/flair/lightning_logs/version_0/checkpoints/latest_checkpoint.ckpt'```)
+You again need to specify the path to the VQ-GAN checkpoint from before (e.g. ```model.vqgan_ckpt='/checkpoints/latest_checkpoint.ckpt'```)
 
-# Train on your own dataset
-To simpify the dataloading for your own dataset, we provide a default dataset that simply requires the path to the folder with your NifTI images inside, i.e.
+## Results
 
-    root_dir/					# Path to the folder that contains the images
-    ├── img1.nii                # The name of the NifTI file is not important
-    ├── img2.nii                    
-    ├── img3.nii                     
-    ├── ...                    
+### Qualitative Results
+The images are real images, labels and synthetic images(from left to right)
 
-All you need to do now is just specify the path to this root directory the way we have dealt with it before, i.e.,
+|![Image 1](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/75_image.gif) <br/>|![Image 2]( /Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/75_label.gif)|![Image 3](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/75_sample.gif)|
+|---|-----------------------------------------------------------------------------------|-----------------------------------------------------------------------------------|
 
-```
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=default dataset.root_dir=<INSERT_PATH_TO_ROOT_DIRECTORY> model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='own_dataset' model.precision=16 model.embedding_dim=8 model.n_hiddens=16 model.downsample=[2,2,2] model.num_workers=32 model.gradient_clip_val=1.0 model.lr=3e-4 model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384 model.accumulate_grad_batches=1 
-```
-Note that you need to provide the path to the dataset (e.g. ```dataset.root_dir='/../../root_dir/'```) to successfully run the command.
+| ![Image 1](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/95_image.gif) <br/> | ![Image 2]( /Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/95_label.gif) | ![Image 3](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/95_sample.gif) |
+|------------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------|
 
-To train the diffusion model in the latent space of the previously trained VQ-GAN model, you need to run the following command
-```
-python train/train_ddpm.py model=ddpm dataset=default model.results_folder_postfix='own_dataset' model.vqgan_ckpt=<INSERT_PATH_TO_CHECKPOINT> model.diffusion_img_size=32 model.diffusion_depth_size=32 model.diffusion_num_channels=8 model.dim_mults=[1,2,4,8] model.batch_size=10 model.gpus=1
-```
-Where you again need to specify the path to the VQ-GAN checkpoint from before (e.g. ```model.vqgan_ckpt='/home/<user>/Desktop/medicaldiffusion/checkpoints/vq_gan/DEFAULT/own_dataset/lightning_logs/version_0/checkpoints/latest_checkpoint.ckpt'```)
+| ![Image 1](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/13_image.gif) <br/> | ![Image 2]( /Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/13_label.gif) | ![Image 3](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/13_sample.gif) |
+|------------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------|
+
+| ![Image 1](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/1_image.gif) <br/> | ![Image 2]( /Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/1_label.gif) | ![Image 3](/Users/tangwenwu/Documents/GitHub/medicaldiffusion/assets/1_sample.gif) |
+|-----------------------------------------------------------------------------------------|------------------------------------------------------------------------------------|------------------------------------------------------------------------------------|
+
+### Progressive Denosing
+
+![model.png](assets/prograssiv.png)
+
+### Quantitative Results
+
+| Dataset  | Decoder | #channels | FID   | 3D-FID | LPIPS | SSIM   | RMSE  | PSNR  |
+|----------|---------|-----------|-------|--------|-------|--------|-------|-------|
+| AutoPET  | normal  | 37        | 200.45| 0.0090 | 0.19  | 0.6307 | 0.13  | 17.75 |
+| AutoPET  | normal  | 64        | 196.78| 0.0090 | 0.19  | 0.8374 | 0.13  | 17.75 |
+| AutoPET  | normal  | 128       | 197.06| 0.0094 | 0.24  | 0.7090 | 0.41  | 7.74  |
+| AutoPET  | SPADE   | 64        | 197.02| 0.0089 | 0.18  | 0.9969 | 0.16  | 15.93 |
+| SynthRAD | normal  | 32        | 200.45| 0.0060 | 0.25  | 0.6307 | 0.32  | 9.91  |
+| SynthRAD | SPADE   | 32        | 198.03| 0.0061 | 0.27  | 0.6147 | 0.36  | 8.89  |
+| Duke     | normal  | 8         | 193.09| 0.0059 | 0.28  | 0.9390 | 0.20  | 14.95 |
+| Duke     | SPADE   | 8         | 193.62| 0.0054 | 0.29  | 0.9271 | 0.21  | 14.87 |
+
+### Synthetic Data can Confuse Segmentation Network
+We train a 3D U-Net on real data and test on both synthetic and real data.
+Hypothesis: if the generated images belong to the same distribution as real image, the segmentation network should produce similar results on both types of images
+![model.png](assets/compare.png)
+
+| Data resource         | Dice    |
+|-----------------------|---------|
+| Real data (train set) | 0.75041 |
+| Real data (test set)  | 0.71496 | 
+| Synthetic Data        | 0.60197 |
 
 
+
+## Conclusion
+
+In this study, we propose a framework Med-LSDM (Latent Semantic Diffusion Model) for 3D semantic image synthesis for medical imaging. The architecture of the network consists of three key components:  a 3D VQ-GAN and a 3D Semantic Diffusion Model. We implement the diffusion model in the learned latent space of VQ-GAN. Our aim is to generate more medical data from semantic map for the down stream tasks, such as semantic image segmentation and recognition. Challenges caused by the complexity and sensitivity of the data involved. 
+
+The effect of our framework was validated on three challenging datasets: AutoPET, SynthRAD2023 and Duke Breast dataset. The proposed framework achieves a  promising results. According to ablation study, we observed that the model performed significantly better on CT images compared to MR images in terms of performance metrics.
+
+Our experiments prove that the model could effectively bridge the domain gap between real and generated data, as evidenced by the high similarity in segmentation results on both real and synthetic images. This finding highlights the potential of using synthetic data to augment real datasets in medical image analysis tasks.
---
+++ vqgan.sh
@@ -0,0 +1,181 @@
+#!/bin/bash -l
+
+#Slurm parameters
+#SBATCH --job-name=3DVQGAN
+#SBATCH --output=VQ-GAN%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=3-23:00:00
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --qos=batch
+#SBATCH --nodelist=linse21
+#SBATCH --qos=shortbatch
+#SBATCH --partition=highperf
+# SBATCH --gpus=rtx_a5000:1
+# SBATCH --nodelist=linse19
+
+
+# Activate everything you need
+
+#conda activate /anaconda3/envs/myenv
+module load cuda
+pyenv activate myenv38 #myenv #venv
+pip uninstall nvidia_cublas_cu11
+nvcc --version
+python -c "import torch; print(torch.__version__)"
+export CUDA_LAUNCH_BLOCKING=1
+
+# Run your python code
+
+
+# autopet
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \  #8
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# autopet_wo_artifacts
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet_wo_artifacts \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# mr_wo_artifacts
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023 \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='mr_final' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=4 model.n_codes=16384
+
+# mr_wo_mask
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023_wo_mask \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# mr_wo_mask
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=totalsegmentator_mri \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# duke_
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# duke_bspline
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_bspline' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384 dataset.root_dir='/misc/data/private/autoPET/duke/mr_bspline' \
+
+# duke_no_rescale
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_randomcrop' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-5 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384  \
+
+# duke_only_t1_real
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_only_t1_real' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=1e-5 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=4 model.n_codes=16384  \
+#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
+
+# duke_only_t1_randomcrop_tanh
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_randomcrop_t1_real_tanh' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real'
+
+# autopet_tanh
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='autopet_tanh' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=10000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# duke_t1_all
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_all' model.precision=32 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_all_rescale_crop'
+
+# duke_t1_all_tanh
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_all_tanh' model.precision=32 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+
+# duke_t1_tanh
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results_t1_real_tanh' model.precision=32 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384  \
+#dataset.root_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop' dataset.val_dir='/misc/data/private/autoPET/duke/T1_MR_real_rescale_crop'
+
+# semantic map
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=semanticmap \
+#model=vq_gan_3d model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# vq_gan_spade  137077
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=autopet \
+#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
+
+# vq_gan_spade synthrad2023
+PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023 \
+model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_mr_tanh' model.precision=16 model.embedding_dim=8 \
+model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+model.gan_feat_weight=4 model.batch_size=2 model.n_codes=16384
+
+# vq_vae seg_map autopet segmap
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=semanticmap \
+#model=vq_vae model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.batch_size=2 model.n_codes=16384
+
+# vq_vae seg_map synthrad mr
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=synthrad2023 \
+#model=vq_vae model.gpus=1 model.default_root_dir_postfix='results' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=3e-4 \
+#model.batch_size=2 model.n_codes=16384
+
+# vq_gan_spade duke
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_duke' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=1e-5 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
+
+# vq_gan_spade_tanh duke
+#PL_TORCH_DISTRIBUTED_BACKEND=gloo python train/train_vqgan.py dataset=duke \
+#model=vq_gan_spade model.gpus=1 model.default_root_dir_postfix='vq_gan_spade_tanh_duke' model.precision=16 model.embedding_dim=8 \
+#model.n_hiddens=16 model.downsample=[4,4,4] model.num_workers=4 model.gradient_clip_val=1.0 model.lr=1e-5 \
+#model.discriminator_iter_start=1000 model.perceptual_weight=4 model.image_gan_weight=1 model.video_gan_weight=1 \
+#model.gan_feat_weight=4 model.batch_size=1 model.n_codes=16384
+
